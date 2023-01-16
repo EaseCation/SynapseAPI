222897b18bac60ba35ac765ff31a186700e65f4c
boybook <fangyizhou1997@qq.com>
2023-01-16 15:14:10 +0800

    1月15日上线：调整整体游戏难度、修复自动发货的问题

--- src/main/java/org/itxtech/synapseapi/SynapsePlayer16.java
+++ src/main/java/org/itxtech/synapseapi/SynapsePlayer16.java
@@ -10,7 +10,6 @@ import cn.nukkit.lang.TranslationContainer;
 import cn.nukkit.level.GameRules;
 import cn.nukkit.level.Level;
 import cn.nukkit.level.Position;
-import cn.nukkit.math.Mth;
 import cn.nukkit.math.Vector3;
 import cn.nukkit.network.SourceInterface;
 import cn.nukkit.network.protocol.*;
@@ -26,20 +25,24 @@ import org.itxtech.synapseapi.event.player.SynapsePlayerBroadcastLevelSoundEvent
 import org.itxtech.synapseapi.event.player.SynapsePlayerConnectEvent;
 import org.itxtech.synapseapi.event.player.netease.NetEasePlayerModEventC2SEvent;
 import org.itxtech.synapseapi.event.player.netease.NetEasePlayerPyRpcReceiveEvent;
+import org.itxtech.synapseapi.event.player.netease.SynapsePlayerNetEaseStoreBuySuccEvent;
 import org.itxtech.synapseapi.multiprotocol.AbstractProtocol;
 import org.itxtech.synapseapi.multiprotocol.PacketRegister;
-import org.itxtech.synapseapi.multiprotocol.protocol14.protocol.*;
+import org.itxtech.synapseapi.multiprotocol.protocol14.protocol.LoginPacket14;
 import org.itxtech.synapseapi.multiprotocol.protocol16.protocol.*;
 import org.itxtech.synapseapi.multiprotocol.utils.LevelSoundEventEnum;
 import org.itxtech.synapseapi.network.protocol.spp.PlayerLoginPacket;
-import org.msgpack.value.*;
+import org.msgpack.value.ArrayValue;
+import org.msgpack.value.MapValue;
+import org.msgpack.value.Value;
+import org.msgpack.value.ValueFactory;
 
 import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 
-import static cn.nukkit.SharedConstants.*;
+import static cn.nukkit.SharedConstants.RESOURCE_PACK_CHUNK_SIZE;
 
 public class SynapsePlayer16 extends SynapsePlayer14 {
 
@@ -189,6 +192,9 @@ public class SynapsePlayer16 extends SynapsePlayer14 {
 									);
 									Server.getInstance().getPluginManager().callEvent(modEventC2SEvent);
 								}
+							} else if ("StoreBuySuccServerEvent".equals(value0.get(0).getAsString())) {
+								SynapsePlayerNetEaseStoreBuySuccEvent ev = new SynapsePlayerNetEaseStoreBuySuccEvent(this);
+								Server.getInstance().getPluginManager().callEvent(ev);
 							}
 						}
 					}
