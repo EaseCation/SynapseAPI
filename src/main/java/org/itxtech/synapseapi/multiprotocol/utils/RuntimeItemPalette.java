c032d0eeb6a0614e06d76c8e5f66a39b1bbef8f4
Woder <17339354+wode490390@users.noreply.github.com>
2026-03-28 10:30:38 +0800

    Remove invalid item runtime data

--- src/main/java/org/itxtech/synapseapi/multiprotocol/utils/RuntimeItemPalette.java
+++ src/main/java/org/itxtech/synapseapi/multiprotocol/utils/RuntimeItemPalette.java
@@ -95,12 +95,12 @@ public class RuntimeItemPalette implements AdvancedRuntimeItemPaletteInterface {
         if (microsoftToNetease) {
             if (protocol.isOlderThan(AbstractProtocol.PROTOCOL_121_50)) {
                 registerItem(new Entry("minecraft:mod_ore", 230, 230, null));
+                registerItem(new Entry("minecraft:mod_armor", 1996, 454, null));
             } else {
 //                unregisterItem("minecraft:chalkboard", 230);
 //                unregisterItem("minecraft:board", 454);
             }
             registerItem(new Entry("minecraft:micro_block", -9735, -9735, null));
-            registerItem(new Entry("minecraft:mod_armor", 1996, 454, null));
             registerItem(new Entry("minecraft:mod", 1997, 456, null));
             registerItem(new Entry("minecraft:mod_ex", 1998, 498, null));
             registerItem(new Entry("minecraft:debug_stick", 1999, 735, null));
