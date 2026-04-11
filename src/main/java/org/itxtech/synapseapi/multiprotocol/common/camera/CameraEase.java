3562aa252758c93ea1a0957a52114bf1db3de5cc
Woder <17339354+wode490390@users.noreply.github.com>
2026-04-11 18:27:18 +0800

    Fix NPE in CameraEase

--- src/main/java/org/itxtech/synapseapi/multiprotocol/common/camera/CameraEase.java
+++ src/main/java/org/itxtech/synapseapi/multiprotocol/common/camera/CameraEase.java
@@ -1,10 +1,7 @@
 package org.itxtech.synapseapi.multiprotocol.common.camera;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import lombok.*;
 import lombok.Builder.Default;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Builder
 @NoArgsConstructor
@@ -12,10 +9,12 @@ import lombok.ToString;
 @ToString
 public class CameraEase {
     @Default
+    @NonNull
     public EasingType type = EasingType.LINEAR;
     public float duration;
 
     public CameraEase(float duration) {
         this.duration = duration;
+        this.type = EasingType.LINEAR;
     }
 }
